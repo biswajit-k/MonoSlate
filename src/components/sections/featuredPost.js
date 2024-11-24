eed071fb8c79ec4aad7dc0160bb13cdb85d88c1f
biswajit-k <biswajitkaushik02@gmail.com>
2024-11-24 21:26:32 +0530

    update post ordering in featured

--- src/components/sections/featuredPost.js
+++ src/components/sections/featuredPost.js
@@ -109,7 +109,7 @@ const FeaturedPost = () => {
           fileAbsolutePath: { regex: "/content/posts/*/" }
           frontmatter: { featured: { eq: true } }
         }
-        sort: { fields: [frontmatter___date], order: ASC }
+        sort: { fields: [frontmatter___date], order: DESC }
       ) {
         edges {
           node {
